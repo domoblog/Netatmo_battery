16c3f23489c73c795f7efe46cca47f46f7d07762
domo-blog <aurel@domo-blog.fr>
2017-02-01 18:16:32 +0100

    Add files via upload

--- stationmeteo_battery.py
+++ stationmeteo_battery.py
@@ -21,7 +21,7 @@ from pprint import pprint
 # Debut du code fourni par Netatmo
 payload = {'grant_type': 'password',
            'username': "IDENTIFIANT Netatmo",
-           'password': "MotDePasse du compte",
+           'password': "MotDePasse",
            'client_id':"ID correspondant à une application créée sur le site de développement Netatmo",
            'client_secret': "MotDePasse correspondant à l'application créée sur le site de développement Netatmo",
            'scope': 'read_station'}
@@ -38,6 +38,8 @@ params = {
     'device_id': 'Adresse MAC de la station météo'
 }
 
+batteries = {}
+
 try:
     response = requests.post("https://api.netatmo.com/api/getstationsdata", params = params)
     response.raise_for_status()
@@ -45,42 +47,25 @@ try:
 # Fin du code fourni par Netatmo
 ################################
 
-# Recuperation du pourcentage restant de la batterie des differents modules
-# Pour permettre le fonctionnement du script avec seulement le module exterieur
-# chaque variable est mise avec la valeur du module par defaut
-    #pprint(data)
+# Recuperation du pourcentage restant de la batterie des differents modules de la station
+    for mod in data[u'devices'][0][u'modules']:
+		assert(u'battery_percent' in mod)
+		percent = mod[u'battery_percent']
+		mod_name = mod[u'module_name']
+		batteries[mod_name] = percent
+		print(mod_name)
+		print(batteries[mod_name])
 
-# Module temperature exterieur
-    assert(u'battery_percent' in data[u'devices'][0][u'modules'][0])
-    percent_ext = data[u'devices'][0][u'modules'][0][u'battery_percent']
-# Module anenometre
-    assert(u'battery_percent' in data[u'devices'][0][u'modules'][0])
-    percent_ano = data[u'devices'][0][u'modules'][0][u'battery_percent']
-# Module pluviometre
-    assert(u'battery_percent' in data[u'devices'][0][u'modules'][0])
-    percent_pluv = data[u'devices'][0][u'modules'][0][u'battery_percent']
-# Module complementaire interieur
-    assert(u'battery_percent' in data[u'devices'][0][u'modules'][0])
-    percent_int = data[u'devices'][0][u'modules'][0][u'battery_percent']
 except requests.exceptions.HTTPError as error:
     print(error.response.status_code, error.response.text)
 
-# creation de la structure XML 
-ext = etree.Element("EXT")
-battery = etree.SubElement(ext, "BATTERY")
-# Module exterieur
-value1 = etree.SubElement(battery, "VALUE1")
-value1.text = str(percent_ext)
-# Module anenometre
-value2 = etree.SubElement(battery, "VALUE2")
-value2.text = str(percent_ano)
-# Module pluviometre
-value3 = etree.SubElement(battery, "VALUE3")
-value3.text = str(percent_pluv)
-# Module interieur
-value4 = etree.SubElement(battery, "VALUE4")
-value4.text = str(percent_int)
+# Creation de la structure XML 
+ext = etree.Element("Ext")
+battery = etree.SubElement(ext, "Battery")
+
+for mod_name in batteries:
+	value = etree.SubElement(battery, mod_name)
+	value.text = str(batteries[mod_name])
 
 fichier = etree.ElementTree(ext)
 fichier.write("stationmeteo_battery.xml")
-
